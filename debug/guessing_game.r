899d7521d831fe3eaa6040524a220ef612d6e7ef
Bob McNabb <robertmcnabb@gmail.com>
2023-03-12 16:28:26 +0000

    add bug-filled guessing game script.

---
+++ debug/guessing_game.r
@@ -0,0 +1,25 @@
+# get a random number for the user to guess
+# using the built-in function sample()
+rand <- smaple(1:100, 1)
+
+# start the game
+cat("Guess a number between 1 and 20.\n")
+
+# something that will start the loop
+guess <- rand
+
+# while the guess is not equal to the number, ask them to guess again
+while (guess != rand) {
+    guess <- readline(prompt="Enter your guess:") # get input using readline()
+    guess <- as.integer(unlist(strsplit(guess, ","))) # make sure it's an integer
+    
+    if (guess > rand) { # if the guess is too high, tell the user and pick again
+        cat("Too low. Guess again.\n")
+    }
+    else {
+        cat("Too high. Guess again.\n")
+    }    
+}
+
+# if they get it right, let them know!
+cat("Congratulations!", guess, "is right!\n")
