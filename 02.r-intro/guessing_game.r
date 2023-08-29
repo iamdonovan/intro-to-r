4b0d631ccfcd446ccfc280c3e729f8205d629563
Bob McNabb <robertmcnabb@gmail.com>
2023-08-29 10:54:05 +0100

    add (one) solution to debugging the guessing game

--- 02.r-intro/guessing_game.r
+++ 02.r-intro/guessing_game.r
@@ -1,22 +1,22 @@
 # get a random number for the user to guess
 # using the built-in function sample()
-rand <- smaple(1:100, 1)
+rand <- sample(1:20, 1)
 
 # start the game
 cat("Guess a number between 1 and 20.\n")
 
 # something that will start the loop
-guess <- rand
+guess <- -1
 
 # while the guess is not equal to the number, ask them to guess again
 while (guess != rand) {
     guess <- readline(prompt="Enter your guess:") # get input using readline()
     guess <- as.integer(unlist(strsplit(guess, ","))) # make sure it's an integer
     
-    if (guess > rand) { # if the guess is too high, tell the user and pick again
+    if (guess < rand) { # if the guess is too high, tell the user and pick again
         cat("Too low. Guess again.\n")
     }
-    else {
+    else if (guess > rand) {
         cat("Too high. Guess again.\n")
     }    
 }
